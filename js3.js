3912ae69efa2f11237e4ca489ce9f0644c8b002e
phillipsalangsang <phillip_salangsang@hotmail.com>
2021-10-20 15:41:21 +0100

    Task 2 re work

--- js3.js
+++ js3.js
@@ -8,5 +8,14 @@ console.log(Boolean(result == 0));
 }
 
 
+while(1){
+let num1 = parseInt(prompt("7"));
+let num2 = parseInt(prompt("8"));
+let result = num1 % num2
+ 
+console.log(Boolean(result == 0));
+}
+
+
 
 
