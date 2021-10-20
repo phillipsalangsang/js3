63603d4ba3ab13da4d77ff449ba921f694f37e58
phillipsalangsang <phillip_salangsang@hotmail.com>
2021-10-20 16:35:43 +0100

    re written

--- js3.js
+++ js3.js
@@ -1,21 +1,26 @@
-let number = promopt("Enter Number:", "")
+// Task 1
+let number = prompt("Enter a number:", "")
+// Taking user input
 
 if (number % 2 == 0){
-    alert("Even Number")
+    alert ("Even number")
 }else{
-    alert("Odd Number")
-}    
-
-let number1 = prompt("Enter number:", "")
-let number2 = prompt("Enter number:", "")
-
-if (number1 / number2 == 0){
-    alert("True")
-}else{
-    alert("False")
+    alert ("Odd number")
 }
 
+// Task 2
+let number1 = prompt("Enter first number:", "")
+let number2 = prompt("Enter second number:", "")
+// Taking user input
+
+if (number1 / number2 == 0){
+    alert ("True")
+}else{
+    alert ("False")
+}
+
+// Task 3
 let students = 344
 let buses = 42
 let answer = Math.floor (students / buses)
-alert("The number of school buses needed" + answer)
+alert ("The number of school buses needed:" + answer)
