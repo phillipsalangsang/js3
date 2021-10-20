ead5a1a3dbcdc0aeaa11761b0cfaf359a4fc2a8e
phillipsalangsang <phillip_salangsang@hotmail.com>
2021-10-20 15:44:38 +0100

    Removed ;

--- js3.js
+++ js3.js
@@ -1,19 +1,19 @@
 while(1) {
-let numIn = parseInt(prompt("6"));
+let numIn = parseInt(prompt("6"))
  
 let result = numIn % 2
  
-console.log(Boolean(!(result == 1)));
-console.log(Boolean(result == 0));
+console.log(Boolean(!(result == 1)))
+console.log(Boolean(result == 0))
 }
 
 
 while(1){
-let num1 = parseInt(prompt("7"));
-let num2 = parseInt(prompt("8"));
+let num1 = parseInt(prompt("7"))
+let num2 = parseInt(prompt("8"))
 let result = num1 % num2
  
-console.log(Boolean(result == 0));
+console.log(Boolean(result == 0))
 }
 
 
