f9d1003fea31cc370163dd6a7ad1982fae9cc59c
phillipsalangsang <phillip_salangsang@hotmail.com>
2021-10-20 15:41:46 +0100

    Final task done

--- js3.js
+++ js3.js
@@ -17,5 +17,13 @@ console.log(Boolean(result == 0));
 }
 
 
-
+let numOfStudents = 344
+let maxCapacity = 42
+ 
+let fullBuses = Math.floor(numOfStudents/ maxCapacity)
+if ((numOfStudents % maxCapacity) != 0) {
+fullBuses++
+}
+ 
+console.log(fullBuses)
 
