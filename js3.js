a433751f9be7c76e7bac2482eafd88facf6d7e42
phillipsalangsang <phillip_salangsang@hotmail.com>
2021-10-21 12:03:20 +0100

    date task

--- js3.js
+++ js3.js
@@ -1,26 +1,63 @@
-// Task 1
-let number = prompt("Enter a number:", "")
-// Taking user input
+// // Task 1
+// let number = prompt("Enter a number:", "")
+// // Taking user input
 
-if (number % 2 == 0){
-    alert ("Even number")
-}else{
-    alert ("Odd number")
-}
+// if (number % 2 == 0){
+//     alert ("Even number")
+// }else{
+//     alert ("Odd number")
+// }
 
-// Task 2
-let number1 = prompt("Enter first number:", "")
-let number2 = prompt("Enter second number:", "")
-// Taking user input
+// // Task 2
+// let number1 = prompt("Enter first number:", "")
+// let number2 = prompt("Enter second number:", "")
+// // Taking user input
 
-if (number1 / number2 == 0){
-    alert ("True")
-}else{
-    alert ("False")
-}
+// if (number1 / number2 == 0){
+//     alert ("True")
+// }else{
+//     alert ("False")
+// }
 
-// Task 3
-let students = 344
-let buses = 42
-let answer = Math.floor (students / buses)
-alert ("The number of school buses needed:" + answer)
+// // Task 3
+// let students = 344
+// let buses = 42
+// let answer = Math.floor (students / buses)
+// alert ("The number of school buses needed:" + answer)
+
+// //Rainbow Task
+// let Rainbow = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"];
+// alert(Rainbow[4]);
+// Rainbow.unshift(Rainbow.pop())
+// console.log(Rainbow)
+
+//Date Task
+let dayNumber = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+alert(dayNumber[0,6])
+
+let dayNumber = 6
+
+if (dayNumber === 0){
+    console.log("Sunday")
+} 
+else if (dayNumber === 1){
+    console.log("Monday")
+} 
+else if (dayNumber === 2){
+    console.log("Tuesday")
+} 
+else if (dayNumber === 3){
+    console.log("Wednesday")
+} 
+else if (dayNumber === 4){
+    console.log("Thursday")
+} 
+else if (dayNumber === 5){
+    console.log("Friday")
+} 
+else if (dayNumber === 6){
+    console.log("Saturday")
+} 
+else {
+    console.log("Wrong Day Number")
+} 
