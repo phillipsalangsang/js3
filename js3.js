5d41b480533ff31d2dcbad00b4e0c52a47619f2e
phillipsalangsang <phillip_salangsang@hotmail.com>
2021-10-21 12:04:58 +0100

    changes to js date

--- js3.js
+++ js3.js
@@ -32,32 +32,32 @@
 // console.log(Rainbow)
 
 //Date Task
-let dayNumber = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-alert(dayNumber[0,6])
+// let dayNumber = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+// alert(dayNumber[0,6])
 
-let dayNumber = 6
+let dayNumber = 6;
 
-if (dayNumber === 0){
-    console.log("Sunday")
-} 
-else if (dayNumber === 1){
-    console.log("Monday")
-} 
-else if (dayNumber === 2){
-    console.log("Tuesday")
-} 
-else if (dayNumber === 3){
-    console.log("Wednesday")
-} 
-else if (dayNumber === 4){
-    console.log("Thursday")
-} 
-else if (dayNumber === 5){
-    console.log("Friday")
-} 
-else if (dayNumber === 6){
-    console.log("Saturday")
-} 
+if (dayNumber === 0) {
+  console.log("Sunday");
+}
+else if (dayNumber === 1) {
+  console.log("Monday");
+}
+else if (dayNumber === 2) {
+  console.log("Tuesday");
+}
+else if (dayNumber === 3) {
+  console.log("Wednesday");
+}
+else if (dayNumber === 4) {
+  console.log("Thursday");
+}
+else if (dayNumber === 5) {
+  console.log("Friday");
+}
+else if (dayNumber === 6) {
+  console.log("Saturday");
+}
 else {
-    console.log("Wrong Day Number")
+  console.log("Invalid day number")
 } 
