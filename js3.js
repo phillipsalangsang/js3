b45ff4e18ee925706410778a4103d88ea10fef29
phillipsalangsang <phillip_salangsang@hotmail.com>
2021-10-21 12:33:23 +0100

    js

--- js3.js
+++ js3.js
@@ -32,7 +32,7 @@
 // console.log(Rainbow)
 
 //Date Task
-let dayNumber = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let Days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 alert(dayNumber[0,6])
 
 let dayNumber = 6;
